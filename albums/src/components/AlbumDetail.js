94aca749bbeb8e9fa2a88b97863e1bc9209ae53a
isaacloft <isaacloft0906@gmail.com>
2018-09-09 23:04:21 +1000

    flexbox card section

--- albums/src/components/AlbumDetail.js
+++ albums/src/components/AlbumDetail.js
@@ -1,16 +1,39 @@
 import React from 'react';
-import { Text } from 'react-native';
+import { Text, Image, View } from 'react-native';
 import Card from './Card.js';
 import CardSection from './CardSection.js';
 
-const AlbumDetail = props => {
+const AlbumDetail = ({ album }) => {
+	const { title, artist, thumbnail_image } = album;
+
 	return (
 		<Card>
 			<CardSection>
-				<Text>{props.album.title}</Text>
+				<View>
+					<Image
+						style={styles.cardThumbNail}
+						source={{ uri: thumbnail_image }}
+					/>
+				</View>
+				<View style={styles.cardTextSection}>
+					<Text>{title}</Text>
+					<Text>{artist}</Text>
+				</View>
 			</CardSection>
 		</Card>
 	);
+};
+
+const styles = {
+	cardTextSection: {
+		flexDirection: 'column',
+		justifyContent: 'space-around',
+		paddingLeft: 10
+	},
+	cardThumbNail: {
+		width: 50,
+		height: 50
+	}
 };
 
 export default AlbumDetail;
