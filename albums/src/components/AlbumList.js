6840cd29dfefbd745c6d60d08a0240ee1557b3ea
isaacloft <isaacloft0906@gmail.com>
2018-09-09 15:41:02 +1000

    lession 42, added card and cardsection components with styling, and use props.children to pass components around

--- albums/src/components/AlbumList.js
+++ albums/src/components/AlbumList.js
@@ -1,13 +1,27 @@
-import React from "react";
-import { Text, View } from "react-native";
+import React, { Component } from 'react';
+import { View } from 'react-native';
+import axios from 'axios';
+import AlbumDetail from './AlbumDetail.js';
 
 
-const AlbumList = () => {
-	return (
-		<View>
-			<Text>some album detail</Text>
-		</View>
-	);
-};
+class AlbumList extends Component {
+	state = { albums: [] };
+
+	componentWillMount() {
+		axios
+			.get('https://rallycoding.herokuapp.com/api/music_albums')
+			.then(response => this.setState({ albums: response.data }));
+	}
+
+	renderAlbums() {
+		return this.state.albums.map(album => (
+			<AlbumDetail key={album.title} album={album} />
+		));
+	}
+	render() {
+		console.log(this.state);
+		return <View>{this.renderAlbums()}</View>;
+	}
+}
 
 export default AlbumList;
